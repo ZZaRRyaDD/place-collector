4c28413537d2dd02a7851c32882c6b55b775a3e3
Nazar <odddellarobea@gmail.com>
2022-04-25 02:20:17 +0700

    Fix requirements files and add invoke-commands

--- tasks.py
+++ tasks.py
@@ -1,4 +1,4 @@
-from curses import panel
+"""Module with invoke-commands."""
 from invoke import Exit, UnexpectedExit, task
 from rich import print, panel
 
@@ -35,20 +35,26 @@ def run(context):
 @task
 def manage(context, command):
     """Template for python manage.py."""
-    return context.run(f"{START_COMMAND} run --rm django python manage.py {command}")
+    return context.run(
+        f"{START_COMMAND} run --rm django python manage.py {command}"
+    )
 
 
 @task
 def startapp(context, app_name):
     """Create new app."""
-    return context.run(f"{START_COMMAND} run --rm django python manage.py startapp {app_name}")
+    return context.run(
+        f"{START_COMMAND} run --rm django python manage.py startapp {app_name}"
+    )
 
 
 @task
 def isort(context, path=DEFAULT_FOLDERS, params=""):
     """Command to fix imports formatting."""
     success("Linters: ISort running")
-    return context.run(f"{START_COMMAND} run --rm django isort {path} {params}")
+    return context.run(
+        f"{START_COMMAND} run --rm django isort {path} {params}"
+    )
 
 
 @task
@@ -75,3 +81,35 @@ def all(context, path=DEFAULT_FOLDERS):
             f"Linters failed: {', '.join(map(str.capitalize, failed))}",
         )
         raise Exit(code=1)
+
+
+@task
+def install_tools(context):
+    """Install cli dependencies, and tools needed to install requirements.
+
+    Define your dependencies here, for example
+    local("sudo npm -g install ngrok")
+    """
+
+    context.run("pip install --upgrade setuptools pip pip-tools wheel")
+
+
+@task
+def install(context, env="development"):
+    """Install local development requirements."""
+    success(f"Install requirements with pip from {env}.txt")
+    context.run(f"pip install -r requirements/{env}.txt")
+
+
+@task
+def compile(context, update=False):
+    """Compile requirements with pip-compile."""
+    success("Compile requirements with pip-compile")
+    upgrade = "-U" if update else ""
+    in_files = [
+        "requirements/production.in",
+        "requirements/development.in",
+        "requirements/base.in",
+    ]
+    for in_file in in_files:
+        context.run(f"pip-compile -q {in_file} {upgrade}")
